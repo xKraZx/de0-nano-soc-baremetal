32c59cb97f72fadae98fbc30b57eeeb9c4145bc7
Krzysztof W. <krzys.wasko@gmail.com>
2026-08-12 15:32:32 +0200

    feat(sw): add runtime FPGA bitstream loading example

--- sw/bare_metal_example.c
+++ sw/bare_metal_example.c
@@ -5,33 +5,97 @@
 #include <alt_bridge_manager.h>
 #include <alt_fpga_manager.h>
 #include <socal.h>
+#include <alt_dma.h>
 
+static ALT_STATUS_CODE fpga_load(const void *fpga_image, size_t fpga_image_size)
+{
+    ALT_STATUS_CODE status;
+
+    printf("INFO: Initializing FPGA manager...\n");
+
+    status = alt_fpga_init();
+    if (status != ALT_E_SUCCESS){
+        printf("ERROR: alt_fpga_init() failed: %" PRIi32 "\n", status);
+        return status;
+    }
+
+    if (alt_fpga_state_get() == ALT_FPGA_STATE_POWER_OFF){
+        printf("ERROR: FPGA is powered off.\n");
+        alt_fpga_uninit();
+        return ALT_E_ERROR;
+    }
+
+    status = alt_fpga_control_enable();
+    if (status != ALT_E_SUCCESS){
+        printf("ERROR: alt_fpga_control_enable() failed: %" PRIi32 "\n", status);
+
+        alt_fpga_uninit();
+        return status;
+    }
+
+    ALT_FPGA_CFG_MODE_t mode = alt_fpga_cfg_mode_get();
+
+    printf("FPGA MSEL mode = %x: ", mode);
+
+    printf("FPGA state = 0x%x\n", alt_fpga_state_get());
+    printf("INFO: Loading FPGA image...\n");
+
+    const uint32_t retries = 5;
+
+    for (uint32_t i = 0; i < retries; ++i){
+        status = alt_fpga_configure(
+            fpga_image,
+            fpga_image_size);
+
+        if (status == ALT_E_SUCCESS)
+            break;
+
+        printf(
+            "WARN: FPGA configuration attempt %u failed: %" PRIi32 "\n",
+            (unsigned int)(i + 1),
+            status);
+    }
+
+    if (status == ALT_E_SUCCESS)
+        printf("INFO: FPGA successfully configured.\n");
+    else
+        printf("ERROR: FPGA configuration failed: %" PRIi32 "\n", status);
+
+    alt_fpga_control_disable();
+    alt_fpga_uninit();
+
+    return status;
+}
 
 ALT_STATUS_CODE socfpga_bridge_setup(ALT_BRIDGE_t bridge)
 {
     ALT_STATUS_CODE status = ALT_E_SUCCESS;
 
+    printf("INFO: Initializing FPGA ...\n");
+
+    if (status == ALT_E_SUCCESS){
+        status = alt_fpga_init();
+    } else {
+        printf("ERROR: Initialization failed.\n");
+        return status;
+    }
+
     printf("INFO: Setup Bridge [%d] ...\n", bridge);
 
-    if (status == ALT_E_SUCCESS)
-    {
+    if (status == ALT_E_SUCCESS){
         status = alt_bridge_init(bridge, NULL, NULL);
     }
 
-    if (status == ALT_E_SUCCESS)
-    {
+    if (status == ALT_E_SUCCESS){
         status = alt_addr_space_remap(ALT_ADDR_SPACE_MPU_ZERO_AT_BOOTROM,
                                       ALT_ADDR_SPACE_NONMPU_ZERO_AT_OCRAM,
                                       ALT_ADDR_SPACE_H2F_ACCESSIBLE,
                                       ALT_ADDR_SPACE_LWH2F_ACCESSIBLE);
     }
 
-    if (status == ALT_E_SUCCESS)
-    {
+    if (status == ALT_E_SUCCESS){
         printf("INFO: Setup of Bridge [%d] successful.\n\n", bridge);
-    }
-    else
-    {
+    } else {
         printf("ERROR: Setup of Bridge [%d] failed. [status = %x ].\n\n", bridge, status);
     }
 
@@ -57,28 +121,35 @@ ALT_STATUS_CODE socfpga_bridge_reg(void)
 
 int main( void )
 {
-    ALT_STATUS_CODE ret = ALT_E_SUCCESS;
+    ALT_STATUS_CODE status = ALT_E_SUCCESS;
     ALT_FPGA_STATE_t status_fpga;
+    ALT_DMA_CHANNEL_t dma_channel;
     alt_wdog_uninit();
 
+    if (status == ALT_E_SUCCESS){
+        extern char rbf_start;
+        extern char rbf_end;
 
-    ret = alt_fpga_init();
+        const char *   fpga_image      = &rbf_start;
+        const uint32_t fpga_image_size = &rbf_end - &rbf_start;
 
-    if (ret != ALT_E_SUCCESS){
-        printf("FPGA manager init failed\n");
+        printf("INFO: FPGA Image binary at %p.\n", fpga_image);
+        printf("INFO: FPGA Image size is %" PRIu32 " bytes.\n", fpga_image_size);
+
+        status = fpga_load(fpga_image, fpga_image_size);
     }
 
-    while ((status_fpga = alt_fpga_state_get()) != ALT_FPGA_STATE_USER_MODE){
-        printf("FPGA is not initialized: [status = %x ].\n\n", status_fpga);
-        for(volatile int i=0; i<2000000; i++);
+    if (status == ALT_E_SUCCESS)
+        printf("INFO: FPGA loading completed.\n");
+    else
+        printf("ERROR: FPGA loading failed.\n");
+
+    if (status == ALT_E_SUCCESS){
+        status = socfpga_bridge_setup(ALT_BRIDGE_H2F);
     }
 
-    if (ret == ALT_E_SUCCESS){
-        ret = socfpga_bridge_setup(ALT_BRIDGE_H2F);
-    }
-
-    if (ret == ALT_E_SUCCESS){
-        ret = socfpga_bridge_reg();
+    if (status == ALT_E_SUCCESS){
+        status = socfpga_bridge_reg();
     }
 
     printf("Hello World!!\n");
