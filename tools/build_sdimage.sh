3fe0c22c5e5d3065b1699ba34bac2255b0fc5ada
Krzysztof W. <krzys.wasko@gmail.com>
2026-09-07 02:49:50 +0200

    refactor(tools): use PROJECT_ROOT in build_sdimage.sh

--- tools/build_sdimage.sh
+++ tools/build_sdimage.sh
@@ -7,6 +7,12 @@ if [ -z "$QUARTUS_ROOTDIR" ]; then
     exit 1
 fi
 
+if [ -z "$PROJECT_ROOT" ]; then
+    echo "----- PROJECT_ROOT is not set:"
+    echo "----- export PROJECT_ROOT=/path/to/this/project/---/"
+    exit 1
+fi
+
 export PATH="$QUARTUS_ROOTDIR/bin:$QUARTUS_ROOTDIR/sopc_builder/bin:$PATH"
 
 
@@ -17,11 +23,11 @@ else
 fi
 
 echo "Preparing files HW..."
-make QUARTUS_ROOTDIR=$QUARTUS_ROOTDIR -C hw all
+make QUARTUS_ROOTDIR=$QUARTUS_ROOTDIR -C $PROJECT_ROOT/hw all
 echo "Preparing files SW..."
-make -C sw all
+make -C $PROJECT_ROOT/sw all
 echo "Creating SD image..."
-$SUDO python3 ./tools/make_sdimage_p3.py \
+$SUDO python3 $PROJECT_ROOT/tools/make_sdimage_p3.py \
   -f \
   -P sw/temp/baremetal-with-spl.sfp,num=1,format=raw,size=10M,type=A2 \
   -s 20M \
